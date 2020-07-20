dac9b1f590facaaa828591737efeb656ce2c2242
张酉夫 <zhangyoufu@gmail.com>
2020-07-20 10:42:53 +0800

    fix version parsing for some special EAP release
    7.7.0-tinymce5-m01

--- check.py
+++ check.py
@@ -8,27 +8,36 @@ class Version(typing.NamedTuple):
     major: int
     minor: int
     patch: int
+    branch: str
     suffix_series: int
     suffix_number: int
-    string: str
+    full_version: str
 
     def __str__(self):
-        return self.string
+        return self.full_version
 
     def __repr__(self):
         return str(self)
 
     @classmethod
     def from_string(self, s):
-        m = re.fullmatch(r'(\d+)\.(\d+)(?:\.(\d+))?(?:-([A-Za-z]+)(\d+))?', s)
+        m = re.fullmatch(r'(\d+)\.(\d+)(?:\.(\d+))?(?:(?:-([a-z0-9]+))?-([A-Za-z]+)(\d+))?', s)
         assert m, s
-        major, minor, patch, suffix_series, suffix_number = m.groups()
+        major, minor, patch, branch, suffix_series, suffix_number = m.groups()
         major = int(major)
         minor = int(minor)
         patch = int(patch) if patch else 0
+        branch = branch or ''
         suffix_series = suffix_series_lut[suffix_series]
         suffix_number = int(suffix_number) if suffix_number else 0
-        return Version(major, minor, patch, suffix_series, suffix_number, s)
+        return Version(major, minor, patch, branch, suffix_series, suffix_number, s)
+
+    @property
+    def branch_version(self):
+        s = f'{self.major}.{self.minor}'
+        if self.branch:
+            s += f'-{self.branch}'
+        return s
 
 session = requests.Session()
 
@@ -80,29 +89,35 @@ def main():
                 version = Version.from_string(version)
                 if version < minimum:
                     continue
+                if channel == 'eap':
+                    assert version.patch == 0
+                else:
+                    assert version.branch == ''
 
-                latest_version, latest_item = latest.setdefault(version.major, {}).setdefault(version.minor, (version, item))
+                latest_version, latest_item = latest.setdefault(version.major, {}).setdefault(version.minor, {}).setdefault(version.branch, (version, item))
                 if latest_version is version:
                     continue
                 if version > latest_version:
-                    latest[version.major][version.minor] = (version, item)
+                    latest[version.major][version.minor][version.branch] = (version, item)
 
         for major in sorted(latest.keys(), reverse=True):
             first = True
             for minor in sorted(latest[major].keys(), reverse=True):
-                _, item = latest[major][minor]
-                tags = [item['version'], f'{major}.{minor}']
-                if first and item['type'] == 'Binary':
-                    first = False
-                    tags.append(f'{major}')
+                for branch in sorted(latest[major][minor].keys()):
+                    version, item = latest[major][minor][branch]
+                    full_version = version.full_version
+                    tags = [version.full_version, version.branch_version]
+                    if first and item['type'] == 'Binary' and branch == '':
+                        first = False
+                        tags.append(f'{major}')
 
-                # sanity check
-                assert item['zipUrl'].startswith('https://')
-                assert re.fullmatch(r'[0-9a-f]{32}', item['md5'])
+                    # sanity check
+                    assert item['zipUrl'].startswith('https://')
+                    assert re.fullmatch(r'[0-9a-f]{32}', item['md5'])
 
-                matrix.append(f'''\
+                    matrix.append(f'''\
         - application: {application}
-          version: {item['version']!r}
+          version: {version.full_version!r}
           url: {item['zipUrl']!r}
           md5: {item['md5']!r}
           tags: {' '.join(tags)!r}''')
